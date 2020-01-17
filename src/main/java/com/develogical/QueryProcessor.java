bc2516a35aadf46f282d080b6cecc3597954b83a
Depasse Arthur <arthur.depasse96@gmail.com>
2020-01-17 16:35:31 +0000

    breaking changes

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -9,7 +9,7 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
         else if (query.toLowerCase().contains("pizzas")) {
-            return "Pizzas are good, yummy, very good !";
+            return "Pizzas are, yummy, very !";
         }
         return "";
     }
